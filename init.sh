98bf378f5cc844dc9c27ad5804cdf5dc24767ed7
PrzemyslawSagalo <przemek@sagalo.pro>
2026-04-17 22:15:17 +0200

    feat: add GitHub Copilot CLI configuration and automated setup

--- init.sh
+++ init.sh
@@ -6,13 +6,22 @@ set -e
 ALIAS_SOURCE_FILE=".bash_aliases"
 ALIAS_DEST_FILE="$HOME/.bash_aliases"
 BASHRC="$HOME/.bashrc"
+COPILOT_CONFIG_DIR=".config/copilot"
+COPILOT_DEST_DIR="$HOME/.config/copilot"
 
 # --- 1. Copy the file ---
 # Assumes you are in the same directory as the source .bash_aliases
 echo "Copying $ALIAS_SOURCE_FILE to $ALIAS_DEST_FILE..."
 cp "$ALIAS_SOURCE_FILE" "$ALIAS_DEST_FILE"
 
-# --- 2. Add sourcing logic to .bashrc ---
+# --- 2. Copy Copilot configuration ---
+if [ -d "$COPILOT_CONFIG_DIR" ]; then
+    echo "Copying $COPILOT_CONFIG_DIR to $COPILOT_DEST_DIR..."
+    mkdir -p "$COPILOT_DEST_DIR"
+    cp -R "$COPILOT_CONFIG_DIR/"* "$COPILOT_DEST_DIR/"
+fi
+
+# --- 3. Add sourcing logic to .bashrc ---
 # This is the line we want to add
 ALIAS_LINE="if [ -f $ALIAS_DEST_FILE ]; then . $ALIAS_DEST_FILE; fi"
 
